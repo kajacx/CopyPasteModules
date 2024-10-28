869c7903b2e2256d048b30639d3182267e86fe6b
kajacx <kajacx@gmail.com>
2024-10-28 18:57:14 +0100

    Try to rework the copy logic

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
 set -e
 
+rm -f functions/*.lua helpers/*.lua
+
 tl check *.tl && tl gen *.tl 
 tl check functions/*tl && tl gen functions/*.tl
 tl check helpers/*tl && tl gen helpers/*.tl
